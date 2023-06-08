ef6881bae22696e26c70d5ecb2d4e04375ca39ee
MrPeterJin <Cheng.Jin@std.uestc.edu.cn>
2023-06-08 10:31:28 +0800

    Init Files

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='kfb',
+    version='1.0.0',
+    author='MrPeterJin',
+    author_email='petergamsing@gmail.com',
+    url='https://chengjin.netlify.app/',
+    description=u'A package to read whole-slide image (WSI) file in .kfb format',
+    packages=find_packages(),
+    }
+)
