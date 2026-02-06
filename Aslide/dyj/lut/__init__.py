b7286fcade26a9b107d37760c9f21312182b2af2
MrPeterJin <Cheng.Jin@std.uestc.edu.cn>
2026-02-06 21:57:09 +0800

    Fix: Surpress setuptools warning

---
+++ Aslide/dyj/lut/__init__.py
@@ -0,0 +1,4 @@
+# This file makes the lut directory a Python package
+# to avoid setuptools warnings during installation.
+# The .lut files in this directory are data files, not Python modules.
+
---
+++ Aslide/mds/icc/__init__.py
@@ -0,0 +1,4 @@
+# This file makes the icc directory a Python package
+# to avoid setuptools warnings during installation.
+# The .icm files in this directory are data files, not Python modules.
+
