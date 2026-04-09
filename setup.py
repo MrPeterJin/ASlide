c5f1297abffb86f5c328597ac629a8d9d46ea754
MrPeterJin <Cheng.Jin@std.uestc.edu.cn>
2026-04-09 12:47:25 +0800

    Fix: Bump package version to 1.6.1
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- setup.py
+++ setup.py
@@ -261,7 +261,7 @@ class CustomInstall(install):
 
 setup(
     name="Aslide",
-    version="1.6.0",
+    version="1.6.1",
     author="MrPeterJin",
     author_email="petergamsing@gmail.com",
     url="https://github.com/MrPeterJin/ASlide",
